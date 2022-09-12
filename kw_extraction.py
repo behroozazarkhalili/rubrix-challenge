aca4ca4cb45f6df7b4723e6f2a31d14170540a5e
ermia <ermiaazarkhalili@gmail.com>
2022-09-12 16:32:08 +0430

    clean the text, and update the keywords extracted from keybert

--- kw_extraction.py
+++ kw_extraction.py
@@ -1,15 +1,17 @@
 import re
+import time
 from collections import Counter
 from typing import List
 
 import pandas as pd
 from keybert import KeyBERT
-from sentence_transformers import SentenceTransformer
+from sentence_transformers import SentenceTransformer, util
 from rapidfuzz.distance import Levenshtein
-from top2vec import Top2Vec
+from transformers import pipeline
 
 # Read the data
 df_train = pd.read_csv('data/yt_comments_train.csv', sep=',', header=0)
+df_test = pd.read_csv('data/yt_comments_test.csv', sep=',', header=0)
 
 
 def get_clean_text(text: str):
@@ -21,21 +23,26 @@ def get_clean_text(text: str):
     return text
 
 
+#  Clean the data
+df_train["clean_text"] = df_train["text"].apply(get_clean_text)
+
+# Remove the column with empty string
+df_train = df_train[df_train["clean_text"] != ""]
+
 # Keyword extraction
 sentence_model = SentenceTransformer("all-MiniLM-L6-v2")
 kw_model = KeyBERT(model=sentence_model)
 
-# Extract keywords
-df_train["clean_text"] = df_train["text"].apply(get_clean_text)
+# Extract keywords by sentence-transformer
 df_train['keywords'] = df_train['clean_text'].apply(lambda x: kw_model.extract_keywords(x, keyphrase_ngram_range=(1, 1), stop_words='english', top_n=5))
 
 # Get the most common keywords
 keywords_list = df_train['keywords'].apply(lambda x: [i[0] for i in x]).tolist()
 keywords_list = [item for sublist in keywords_list for item in sublist]
-keywords_most_frequent = Counter(keywords_list).most_common(10)
+keywords_most_frequent = Counter(keywords_list).most_common(15)
 
-ham_keywords = ["video", "song", "love", "music"]
-spam_keywords = ["subscribe", "channel", "check", "views", "like"]
+ham_keywords = ["song", "love", "music", "like"]
+spam_keywords = ["subscribe", "channel", "check"]
 
 
 # Get the most similar words of the text to the keywords
