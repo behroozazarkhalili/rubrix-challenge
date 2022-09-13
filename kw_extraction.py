6a682989ac783550ad839f5a64c20ca65dced68a
ermia <ermiaazarkhalili@gmail.com>
2022-09-13 10:13:43 +0430

    clean the code when getting keywords.

--- kw_extraction.py
+++ kw_extraction.py
@@ -42,39 +42,43 @@ def get_clean_data(train_dataframe: pd.DataFrame, test_dataframe: pd.DataFrame):
 df_test, df_train = get_clean_data(df_train, df_test)
 
 # Keyword extraction
-sentence_model = SentenceTransformer("all-MiniLM-L6-v2")
-kw_model = KeyBERT(model=sentence_model)
+# sentence_model = SentenceTransformer("all-MiniLM-L6-v2")
+sentence_model = SentenceTransformer("facebook/bart-large-mnli")
+keybert_model = KeyBERT(model=sentence_model)
 
 
-def get_kw(kw_model: KeyBERT, text_df: pd.DataFrame, top_n: int = 5, n_common: int = 5):
+def get_kw(kw_model: KeyBERT, train_dataframe: pd.DataFrame, top_n: int = 5, n_common: int = 5):
     # Extract keywords by sentence-transformer
-    df_train['keywords'] = df_train['clean_text'].apply(lambda x: kw_model.extract_keywords(x, keyphrase_ngram_range=(1, 1), stop_words='english', top_n=5))
+    train_dataframe['keywords'] = train_dataframe['clean_text'].apply(lambda x: kw_model.extract_keywords(x, keyphrase_ngram_range=(1, 1), stop_words='english', top_n=top_n))
 
     # Get the most common keywords
-    keywords_list = df_train['keywords'].apply(lambda x: [i[0] for i in x]).tolist()
+    keywords_list = train_dataframe['keywords'].apply(lambda x: [i[0] for i in x]).tolist()
     keywords_list = [item for sublist in keywords_list for item in sublist]
     keywords_most_frequent = Counter(keywords_list).most_common(n_common)
 
     return keywords_most_frequent
 
 
+frequent_keywords = get_kw(keybert_model, df_train, top_n=5, n_common=5)
 ham_keywords = ["song", "love", "music", "like"]
 spam_keywords = ["subscribe", "channel", "check"]
 
 
 # Get the most similar words of the text to the keywords
 def get_similarity(metric, keywords: List[str], text: str, threshold: float = 0.75):
-    sim_metric = metric.normalized_similarity
-    words = text.split()
-    sim_values = [word for word in words for key_word in keywords if sim_metric(key_word, word) >= threshold]
+    #  check whether metric has the method normalized_similarity
+    if hasattr(metric, 'normalized_similarity'):
+        sim_metric = metric.normalized_similarity
+        words = text.split()
+        sim_values = [word for word in words for key_word in keywords if sim_metric(key_word, word) >= threshold]
+    else:
+        raise ValueError("The metric does not have the method normalized_similarity")
 
     return sim_values
 
 
 #  Zero shot classification
-classifier = pipeline("zero-shot-classification",
-                      model="facebook/bart-large-mnli")
-
+classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
 zs_labels = classifier(df_train["clean_text"].iloc[0:10].tolist(), ham_keywords + spam_keywords, multi_label=False)
 
 #  Clustering
