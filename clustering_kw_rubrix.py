116453320c2709d0ec0462b8a636280bab477ba9
ermia <ermiaazarkhalili@gmail.com>
2022-09-13 19:03:45 +0430

    clean the code and add document to all the functions.

--- clustering_kw_rubrix.py
+++ clustering_kw_rubrix.py
@@ -9,7 +9,6 @@ import pandas as pd
 from keybert import KeyBERT
 from rubrix.labeling.text_classification import WeakLabels, load_rules, MajorityVoter
 from sentence_transformers import SentenceTransformer, util
-from rapidfuzz.distance import Levenshtein
 from sklearn.metrics import classification_report
 from transformers import pipeline
 
@@ -18,8 +17,12 @@ df_train = pd.read_csv('data/yt_comments_train.csv', sep=',', header=0, index_co
 df_test = pd.read_csv('data/yt_comments_test.csv', sep=',', header=0, index_col=0)
 
 
-def get_clean_text(text: str):
-    """remove the https address, email, emoji, html tag, and number form the text"""
+def get_clean_text(text):
+    """
+    remove the https address, email, emoji, html tag, and number form the text
+    :param str text: the text to be cleaned
+    :return: return the cleaned text
+    """
     text = re.sub(r"http\S+", "", text)
     text = re.sub(r"@\S+", "", text)
     text = re.sub(r"\d{2,}", "", text)
@@ -32,8 +35,14 @@ def get_clean_text(text: str):
     return text
 
 
-#  Clean the data
-def get_clean_data(train_dataframe: pd.DataFrame, test_dataframe: pd.DataFrame):
+#  Clean the dataframes
+def get_clean_data(train_dataframe, test_dataframe):
+    """
+    Clean the data
+    :param pd.DataFrame train_dataframe: the train dataframe
+    :param pd.DataFrame test_dataframe: the test dataframe
+    :return: return the cleaned dataframes
+    """
     train_dataframe['clean_text'] = train_dataframe['text'].apply(get_clean_text)
     test_dataframe['clean_text'] = test_dataframe['text'].apply(get_clean_text)
 
@@ -46,17 +55,33 @@ def get_clean_data(train_dataframe: pd.DataFrame, test_dataframe: pd.DataFrame):
 
 df_train, df_test = get_clean_data(df_train, df_test)
 
+
+def get_kw_model(model_name_path: str = "all-MiniLM-L6-v2"):
+    """
+    Get the keyword extraction model
+    :param str model_name_path: the model name or path
+    :return: return the keyword extraction model
+    """
+    sentence_model = SentenceTransformer(model_name_or_path=model_name_path)
+    kw_model = KeyBERT(model=sentence_model)
+    return kw_model, sentence_model
+
+
 # Keyword extraction
-sentence_model = SentenceTransformer("all-MiniLM-L6-v2")
-keybert_model = KeyBERT(model=sentence_model)
+keybert_model, _ = get_kw_model()
 
 
-def get_kw(kw_model: KeyBERT, train_dataframe: pd.DataFrame, top_n: int = 5):
-    # Extract keywords by sentence-transformer
-    train_dataframe['keywords'] = train_dataframe['clean_text'].apply(lambda x: kw_model.extract_keywords(x, keyphrase_ngram_range=(1, 1), stop_words='english', top_n=top_n))
+def get_kw(kw_model, df, top_n=5):
+    """
+    Extract keywords by sentence-transformer
+    :param KeyBERT kw_model: the keyword extraction model
+    :param pd.DataFrame df: the dataframe
+    :param int top_n: the number of keywords to extract
+    """
+    df['keywords'] = df['clean_text'].apply(lambda x: kw_model.extract_keywords(x, keyphrase_ngram_range=(1, 1), stop_words='english', top_n=top_n))
 
     # Get the most common keywords
-    keywords_list = train_dataframe['keywords'].apply(lambda x: [i[0] for i in x]).tolist()
+    keywords_list = df['keywords'].apply(lambda x: [i[0] for i in x]).tolist()
     keywords_list = [item for sublist in keywords_list for item in sublist]
 
     return keywords_list
@@ -64,13 +89,20 @@ def get_kw(kw_model: KeyBERT, train_dataframe: pd.DataFrame, top_n: int = 5):
 
 frequent_keywords = get_kw(keybert_model, df_train, top_n=5)
 keywords_most_frequent = Counter(frequent_keywords).most_common(10)
+
 ham_keywords = ["song", "love", "music", "like"]
 spam_keywords = ["subscribe", "channel", "check", "youtube"]
 
 
 # Get the most similar words of the text to the keywords
-def get_similarity(metric, keywords: List[str], text: str, threshold: float = 0.75):
-    #  check whether metric has the method normalized_similarity
+def get_similarity(metric, keywords, text, threshold=0.75):
+    """
+    check whether metric has the method normalized_similarity
+    :param metric: the metric to check the similarity
+    :param List[str] keywords: the keywords to check the similarity
+    :param str text: the text to check the similarity
+    :param float threshold: the threshold to check the similarity
+    """
     if hasattr(metric, 'normalized_similarity'):
         sim_metric = metric.normalized_similarity
         words = text.split()
@@ -81,20 +113,14 @@ def get_similarity(metric, keywords: List[str], text: str, threshold: float = 0.
     return sim_values
 
 
-#  Zero shot classification
-classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
-zs_labels = classifier(df_train["clean_text"].iloc[0:10].tolist(), ham_keywords + spam_keywords, multi_label=False)
-
 #  Clustering
 corpus_sentences = df_train["clean_text"].tolist()
 print("Start clustering")
 start_time = time.time()
 
-corpus_embeddings = sentence_model.encode(corpus_sentences, batch_size=64, show_progress_bar=True, convert_to_tensor=True)
+sentence_model = SentenceTransformer("all-MiniLM-L6-v2")
+corpus_embeddings = sentence_model.encode(corpus_sentences, batch_size=64, convert_to_tensor=True)
 
-# Two parameters to tune:
-# min_cluster_size: Only consider cluster that have at least 25 elements
-# threshold: Consider sentence pairs with a cosine-similarity larger than threshold as similar
 clusters = util.community_detection(corpus_embeddings, min_community_size=10, threshold=0.75)
 
 print("Clustering done after {:.2f} sec".format(time.time() - start_time))
@@ -111,7 +137,13 @@ for i, cluster in enumerate(clusters):
         print("\t", corpus_sentences[sentence_id])
 
 
-def get_kw_from_cluster(kw_model: KeyBERT, text_list: List[List[str]], top_n: int = 3):
+def get_kw_from_cluster(kw_model, text_list, top_n=3):
+    """
+    Extract keywords from the cluster
+    :param KeyBERT kw_model: the keyword extraction model
+    :param List[List[str]] text_list: the list of text
+    :param int top_n: the number of keywords to extract
+    """
     initial_kws = [kw_model.extract_keywords(text, keyphrase_ngram_range=(1, 1), stop_words='english', top_n=top_n) for text in text_list]
     kws_list = [[k[0] for kw in cluster for k in kw] for cluster in initial_kws]
     return kws_list
@@ -125,7 +157,7 @@ cluster_most_frequent_kws = [[item[0] for item in most_frequent_kws] for most_fr
 cluster_ham_keywords = ["song", "love", "best"]
 cluster_spam_keywords = ["subscribe", "channel", "check", "playlist"]
 
-# build records from the train dataset
+# Build records from the train dataset
 records = [
     rb.TextClassificationRecord(
         text=row.clean_text,
@@ -134,86 +166,31 @@ records = [
     for i, row in df_train.iterrows()
 ]
 
-# build records from the test dataset with annotation
-labels = ["HAM", "SPAM"]
 
-df_train["label"] = df_train["label"].astype(int)
-records += [
-    rb.TextClassificationRecord(
-        text=row.clean_text,
-        annotation=labels[row.label],
-        metadata={"video": row.video, "author": row.author}
-    )
-    for i, row in df_train.iterrows()
-]
-
-# log records to Rubrix
-rb.log(records, name="weak_supervision_yt")
-
-
-# rules defined as Python labeling functions
-def contains_subscribe(record: rb.TextClassificationRecord):
-    if "subscribe" in record.inputs["text"]:
-        return "SPAM"
-
-
-def contains_check(record: rb.TextClassificationRecord):
-    if "check" in record.inputs["text"]:
-        return "SPAM"
-
-
-def contains_channel(record: rb.TextClassificationRecord):
-    if "channel" in record.inputs["text"]:
-        return "SPAM"
-
-
-def contains_playlist(record: rb.TextClassificationRecord):
-    if "playlist" in record.inputs["text"]:
-        return "SPAM"
-
-
-def contains_youtube(record: rb.TextClassificationRecord):
-    if "youtube" in record.inputs["text"]:
-        return "SPAM"
-
-
-def contains_song(record: rb.TextClassificationRecord):
-    if "song" in record.inputs["text"]:
-        return "HAM"
-
-
-def contains_love(record: rb.TextClassificationRecord):
-    if "love" in record.inputs["text"]:
-        return "HAM"
-
-
-def contains_best(record: rb.TextClassificationRecord):
-    if "best" in record.inputs["text"]:
-        return "HAM"
-
-
-rules = [contains_subscribe, contains_check, contains_channel, contains_song, contains_love, contains_best]
-rules += load_rules(dataset=df_train)
-
-weak_labels = WeakLabels(rules=rules, dataset=df_train)
-
-# show some stats about the rules, see the `summary()` docstring for details
-weak_labels.summary()
-
-# instantiate the majority vote label model by simply providing the weak labels object
-majority_model = MajorityVoter(weak_labels)
-
-# check its performance
-print(majority_model.score(output_str=True))
-
-
-def get_cluster_similarity(cluster_info_list: List[List[str]], text: str):
+# Develop new model based on clustering algorithm
+def get_cluster_similarity(cluster_info_list, text):
+    """
+    Get the similarity between the text and each cluster
+    :param List[List[str]] cluster_info_list: the list of cluster information which can be either list of text or keywords of each cluster
+    :param str text:
+    :return: return the similarity between the text and each cluster
+    """
+    # Encode the list of info in each cluster
     cluster_embeddings_list = [sentence_model.encode(cluster_info, convert_to_tensor=True) for cluster_info in cluster_info_list]
+
+    # Encode the text
     text_embeddings = sentence_model.encode(text, batch_size=64, convert_to_tensor=True)
 
+    # Compute the similarity between the text and the list of info in each cluster
     similarity_matrices = [util.cos_sim(text_embeddings, cluster_embedding).cpu().numpy() for cluster_embedding in cluster_embeddings_list]
+
+    # Get the average similarity score between the text and each cluster
     similarity_scores = [np.mean(similarity_matrix) for similarity_matrix in similarity_matrices]
+
+    # Get the normalized similarity score between the text and each cluster
     normalized_similarity_scores = [score / np.sum(similarity_scores) for score in similarity_scores]
+
+    #  Sort the clusters based on the similarity score
     similar_clusters = np.argsort(similarity_scores)[::-1]
     return normalized_similarity_scores, similar_clusters.tolist()
 
@@ -225,25 +202,59 @@ labels = ["HAM", "SPAM"]
 cluster_labels = ["SPAM", "SPAM", "SPAM", "HAM", "HAM", "SPAM", "HAM", "HAM"]
 
 
-def get_indices(cluster_class_labels: List[str], class_labels: List[str]):
+def get_indices(cluster_class_labels, class_labels):
+    """
+    Get the dict of indices of the clusters that belong to the specific class label
+    :param List[str] cluster_class_labels: the list of class labels of clusters
+    :param List[str] class_labels: the list of unique class labels
+    :return: return the dict of indices of the clusters that belong to the specific class label
+    """
     indices = {label: [index for index in range(len(cluster_class_labels)) if cluster_class_labels[index] == label] for label in class_labels}
     return indices
 
 
 def get_class_labels(clusters_info: List[List[str]], text: str, cluster_class_labels: List[str], class_labels: List[str]):
+    """
+    Get the class label of the text based on the clusters information
+    :param clusters_info: the list of cluster information which can be either list of text or keywords of each cluster
+    :param text: the text to be classified
+    :param cluster_class_labels: the list of class labels of clusters
+    :param class_labels: the list of unique class labels
+    :return: return the class label of the text based on the clusters information
+    """
+    # Get the similarity score between the text and each cluster
     similarity_scores, similar_clusters = get_cluster_similarity(clusters_info, text)
+
+    # Get the dict of indices of the clusters that belong to the specific class label
     labels_indices = get_indices(cluster_class_labels, class_labels)
-    class_labels_scores = {label: sum([similarity_scores[index] for index in indices]) for label, indices in labels_indices.items()}
+
+    # Get the similarity score of each specific class label
+    class_labels_scores = {label: round(sum([similarity_scores[index] for index in indices]), 3) for label, indices in labels_indices.items()}
     return class_labels_scores
 
 
-get_class_labels(cluster_most_frequent_kws, df_test["clean_text"].iloc[0], cluster_labels, labels)
+print(get_class_labels(cluster_most_frequent_kws, df_test["clean_text"].iloc[0], cluster_labels, labels))
 
 
 def get_df_class_labels(df: pd.DataFrame, clusters_info: List[List[str]], cluster_class_labels: List[str], class_labels: List[str]):
+    """
+    Get the class labels of the text column of a dataframe based on the clusters information
+    :param pd.DataFrame df: the dataframe
+    :param List[List[str]] clusters_info: the list of cluster information which can be either list of text or keywords of each cluster
+    :param List[str] cluster_class_labels: the list of class labels of clusters
+    :param List[str] class_labels: the list of unique class labels
+    :return: List[str] return the class labels of the text column of a dataframe based on the clusters information
+    """
+    # Copy the dataframe
     df_class_labels = df.copy()
-    df_class_labels["class_label_scores"] = df_class_labels["clean_text"].apply(lambda text: get_class_labels(clusters_info, text, cluster_class_labels, class_labels)[0])
+
+    # Add the column which is the dict of the scores of class labels
+    df_class_labels["class_label_scores"] = df_class_labels["clean_text"].apply(lambda text: get_class_labels(clusters_info, text, cluster_class_labels, class_labels))
+
+    # Add the column which is the class label
     df_class_labels["class_label"] = df_class_labels["class_label_scores"].apply(lambda class_labels_scores: max(class_labels_scores, key=class_labels_scores.get))
+
+    #  Explode the dict column of the scores of class labels
     df_final = pd.concat([df_class_labels, df_class_labels['class_label_scores'].apply(pd.Series)], axis=1).drop('class_label_scores', axis=1)
     return df_final
 
